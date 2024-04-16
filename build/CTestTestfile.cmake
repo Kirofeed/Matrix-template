ab8511a842df304e87d62b24d29fae3780b25ed7
drozh <Drozhzhakirill@gmail.com>
2024-04-16 23:12:04 +0300

    feat: add diff. methods

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/kirilldrozhzha/proga/matrix-template
-# Build directory: /Users/kirilldrozhzha/proga/matrix-template/build
+# Source directory: C:/Users/drozh/proga/Matrix-template
+# Build directory: C:/Users/drozh/proga/Matrix-template/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
